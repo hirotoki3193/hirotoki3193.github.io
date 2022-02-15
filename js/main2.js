25e2901e9b0e7936672dc6c679993d7eb8f78955
levelco <86658616+levelco@users.noreply.github.com>
2022-02-15 11:33:21 +0900

    Add files via upload
    flexアニメーションを追加

--- js/main2.js
+++ js/main2.js
@@ -1,29 +1,57 @@
 $(function(){
 
-	const getFlag = (checkbox) =>{
+	const getChecked = (checkbox) =>{
 		return checkbox.prop('checked');
 	}
 
-	let	$hero = $('.hero'),
-		$heroText =  $('.hero form label'),
-		$heroChk = $('#hero-chk');
+	let	$heroChk = $('#hero-chk');
 
-	$heroText.on('click',function(){
-		if(getFlag($heroChk)){
+
+	$('.hero label').on('click',function(){
+		if(getChecked($heroChk)){
 			$('.main,.arrow').hide().removeClass('active');
 		}else{
 			$('.main,.arrow').addClass('active').show();
+
 		}
 	});
-	$hero.on('click','img',function(){
-		if(getFlag($heroChk)){
+	$('#img').on('click',function(){
+		if(getChecked($heroChk)){
 			$('.main,.arrow').hide().removeClass('active');
-			$heroChk.removeAttr('checked').prop('checked', false).change();
+			$heroChk.removeAttr('checked').prop('checked',false).change();
 		}
-	})
-
+	});
 	// 固定用
 	$('.years').on('click',function(){
 		$(this).toggleClass('click');
+	});
+
+
+	let $sections = $('section');
+
+	// クラスshowを入れ替える
+	toggleShow = (elem) =>{
+		if($(elem).hasClass('show')){
+			$sections.removeClass('show');
+		}else{
+			$sections.removeClass('show');
+			$(elem).addClass('show');
+		}
+	};
+
+	// 
+	$sections.on('click',function(){
+		let id = '#' + $(this).attr('id');
+		toggleShow(id);
+	});
+
+	// ヘッダーとリンク
+	$('.main ul a').each(function(){
+		$(this).on('click',function(e){
+			// e.preventDefault();
+			let id = $(this).attr('href');
+			toggleShow(id);
+
+		});
 	});
 });
